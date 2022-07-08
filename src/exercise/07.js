c18e4b5177f677224b3835be287f9bc0df43afb2
Stephanie Chan <103970034+eina-chan@users.noreply.github.com>
2022-07-08 17:08:54 +1000

    Excercise 7 - additional note following sol vid

--- src/exercise/07.js
+++ src/exercise/07.js
@@ -30,7 +30,7 @@ function App() {
       <ul style={{listStyle: 'none', paddingLeft: 0}}>
         {items.map(item => (
           // 🐨 add a key prop to the <li> below. Set it to item.id
-          <li key={item.id}>
+          <li key={item.id}> {/* NOTE: Use of indexes is default functionality so won't be recognised as a unique value! */}
             <button onClick={() => removeItem(item)}>remove</button>{' '}
             <label htmlFor={`${item.id}-input`}>{item.value}</label>{' '}
             <input id={`${item.id}-input`} defaultValue={item.value} />
